ecced9dc72e4ac665e15b01de8cbb1d73e6b990c
Gustavo Martín <flaco@flacomac.local>
2011-12-26 22:06:59 -0200

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  
-  get "mercado_pago_notification/receive"
-  match "/mercado_pago_no/show" => "mercado_pago#show", :as => :show_me_the_money 
-  
+
   resources :orders do
     resource :checkout, :controller => 'checkout' do
       member do
